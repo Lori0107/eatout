9520fbf27400d5ba0d042eb46572c0091b72c403
Lori0107 <lori.benzemra@gmail.com>
2020-08-25 15:34:46 +0200

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,35 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <GmapMap
+      id="#map"
+      :center="center"
+      :zoom="7"
+      map-type-id="terrain"
+      style="width: 500px; height: 300px"
+    ></GmapMap>
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      center: { lat: 45.508, lng: -73.587 }
+    }
+  },
+  mounted() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log(position.coords.latitude, position.coords.longitude);
+    });
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+#map {
+  height: 500px;
+  width: 500px;
+  border: 2px solid lightgrey;
+  border-radius: 5px
 }
 </style>
